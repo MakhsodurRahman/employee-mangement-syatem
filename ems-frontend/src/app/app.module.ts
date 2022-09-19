c3e5b182fe63004a209d4974128862d27b02c847
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-09-19 11:29:01 +0600

    import form class

--- ems-frontend/src/app/app.module.ts
+++ ems-frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { CreateEmployeeComponent } from './components/create-employee/create-emp
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
