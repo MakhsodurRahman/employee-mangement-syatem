57c98e7c381a8e561dcf8e700fd051e48cfb6e31
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-09-19 11:46:27 +0600

    add fingById method in controller

--- employee-management-system/src/main/java/com/ems/controller/EmployeeController.java
+++ employee-management-system/src/main/java/com/ems/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.ems.controller;
 
 import com.ems.entity.Employee;
+import com.ems.exception.ResourceNotFoundException;
 import com.ems.repository.EmployeeRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -23,5 +26,10 @@ public class EmployeeController {
     @PostMapping("employees")
     public void addEmployee(@RequestBody Employee employee){
         employeeRepository.save(employee);
+    }
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException(id +" this id not found"));
+        return ResponseEntity.ok(employee);
     }
 }
