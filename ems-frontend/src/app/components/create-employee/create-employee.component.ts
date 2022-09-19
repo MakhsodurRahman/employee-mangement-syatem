5ebd71bae7eafd789babd209245760faf4b76614
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-09-19 11:27:39 +0600

    add create employee functionality

--- ems-frontend/src/app/components/create-employee/create-employee.component.ts
+++ ems-frontend/src/app/components/create-employee/create-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { Employee } from 'src/app/AllClass/employee';
+import { EmployeeService } from 'src/app/service/employeeService';
 
 @Component({
   selector: 'app-create-employee',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateEmployeeComponent implements OnInit {
 
-  constructor() { }
+  employee: Employee = new Employee();
+  constructor(private emplyeeService: EmployeeService,
+    private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  saveEmployee(){
+    this.emplyeeService.createEmployee(this.employee).subscribe(data =>{
+      console.log(data);
+      this.gotToEmployeeList();
+    },
+    error => console.log(error)
+    
+    );
+  }
+
+  gotToEmployeeList(){
+    this.router.navigate(['/employees']);
+  }
+  onSubmit(){
+    console.log(this.employee);
+    this.saveEmployee();
   }
 
 }
